bc973685cad3d39a1e478d5c410bbb6cd6dbbe75
Steve Hollasch <steve@hollasch.net>
2024-11-13 12:40:16 -0600

    ray4: Eliminate large goto statement

--- ray4/src/r4_trace.cpp
+++ ray4/src/r4_trace.cpp
@@ -92,103 +92,101 @@ void RayTrace (
 
     double NdotD = 0;  // Normal dot ray direction
 
-    if (!(nearattr->flags & (AT_DIFFUSE | AT_SPECULAR)))
-        goto REFTRAN;
+    if (nearattr->flags & (AT_DIFFUSE | AT_SPECULAR)) {
 
-    // If we're looking at the object from `behind' (or inside), then flip the normal vector.
+        // If we're looking at the object from `behind' (or inside), then flip the normal vector.
 
-    NdotD = dot(nearnormal, ray.direction);
+        NdotD = dot(nearnormal, ray.direction);
 
-    if (NdotD > 0.0) {
-        nearnormal = -nearnormal;
-        NdotD = -NdotD;
-    }
-
-    // To avoid surface acne, move the intersection point just outside the object surface to prevent
-    // that same surface from erroneously shadowing itself.
-
-    Point4 intr_out = nearintr + (1e-10 * nearnormal);  // Intersection Outside The Surface
-
-    // Add illumation to the point from all visible lights.
-
-    for (auto *light = lightlist;  light;  light=light->next) {
-        Vector4 ldir;     // Light Direction
-        double  mindist;  // Nearest Object Distance
-
-        if (light->type == LightType::Directional) {
-            ldir = light->direction;
-            mindist = -1.0;
-        } else {
-            double norm;  // Vector Norm
-
-            ldir = light->position - intr_out;
-
-            // Normalize the light-direction vector. If the (point) light source is VERY close to
-            // the intersection point, then just set the light-direction vector to the surface
-            // normal.
-
-            mindist = norm = ldir.norm();
-            if (norm < epsilon)
-                ldir = nearnormal;
-            else
-                ldir /= norm;
+        if (NdotD > 0.0) {
+            nearnormal = -nearnormal;
+            NdotD = -NdotD;
         }
 
-        // Determine if the light is obscurred by any other object. If the light is obscurred by a
-        // transparent object, then ignore refraction, but color the light we're receiving based on
-        // the object's transparent color.
+        // To avoid surface acne, move the intersection point just outside the object surface to
+        // prevent that same surface from erroneously shadowing itself.
 
-        auto lcolor = light->color;  // Light Color
+        Point4 intr_out = nearintr + (1e-10 * nearnormal);  // Intersection Outside The Surface
 
-        for (optr=objlist;  optr;  optr=optr->next) {
-            double minsave=mindist;  // Nearest Object Distance (saved)
+        // Add illumation to the point from all visible lights.
 
-            if ((*optr->intersect)(optr, Ray4(intr_out, ldir), &mindist, nullptr, nullptr)) {
-                if (!(optr->attr->flags & AT_TRANSPAR))
-                    break;
+        for (auto *light = lightlist;  light;  light=light->next) {
+            Vector4 ldir;     // Light Direction
+            double  mindist;  // Nearest Object Distance
 
-                lcolor *= optr->attr->Kt;
-                mindist = minsave;
+            if (light->type == LightType::Directional) {
+                ldir = light->direction;
+                mindist = -1.0;
+            } else {
+                double norm;  // Vector Norm
+
+                ldir = light->position - intr_out;
+
+                // Normalize the light-direction vector. If the (point) light source is VERY close
+                // to the intersection point, then just set the light-direction vector to the
+                // surface normal.
+
+                mindist = norm = ldir.norm();
+                if (norm < epsilon)
+                    ldir = nearnormal;
+                else
+                    ldir /= norm;
+            }
+
+            // Determine if the light is obscurred by any other object. If the light is obscurred by
+            // a transparent object, then ignore refraction, but color the light we're receiving
+            // based on the object's transparent color.
+
+            auto lcolor = light->color;  // Light Color
+
+            for (optr=objlist;  optr;  optr=optr->next) {
+                double minsave=mindist;  // Nearest Object Distance (saved)
+
+                if ((*optr->intersect)(optr, Ray4(intr_out, ldir), &mindist, nullptr, nullptr)) {
+                    if (!(optr->attr->flags & AT_TRANSPAR))
+                        break;
+
+                    lcolor *= optr->attr->Kt;
+                    mindist = minsave;
+                }
+            }
+
+            // If an opaque object shadows us, then skip this light source. Also, if the maximum
+            // amount of light transmitted through transparent objects is less than 1/256, then this
+            // light source can add nothing significant, so skip it.
+
+            if ((optr) || ((lcolor.r + lcolor.g + lcolor.b) < 0.001))
+                continue;
+
+            // If surface normal is turned from light, skip this light.
+
+            double ftemp = dot(nearnormal, ldir);  // Scratch Real Value
+            if (ftemp <= 0.0)
+                continue;
+
+            // Add the diffuse component of the light.
+
+            color += ftemp * nearattr->Kd * lcolor;
+
+            // If this object has no specular reflection, do next light.
+
+            if (!(nearattr->flags & AT_SPECULAR))  continue;
+
+            // Calculate the light reflection vector.
+
+            ftemp *= 2.0;
+            Vector4 Refl = -ldir + (ftemp * nearnormal);  // Reflection Vector
+
+            // Calculate the cosine of the sight & reflection vectors, raised to the Phong power,
+            // for the specular reflection.
+
+            ftemp = dot(-ray.direction, Refl);
+            if (ftemp > 0.0) {
+                ftemp = pow (ftemp, nearattr->shine);
+                color += ftemp * nearattr->Ks * lcolor;
             }
         }
-
-        // If an opaque object shadows us, then skip this light source. Also, if the maximum amount
-        // of light transmitted through transparent objects is less than 1/256, then this light
-        // source can add nothing significant, so skip it.
-
-        if ((optr) || ((lcolor.r + lcolor.g + lcolor.b) < 0.001))
-            continue;
-
-        // If surface normal is turned from light, skip this light.
-
-        double ftemp = dot(nearnormal, ldir);  // Scratch Real Value
-        if (ftemp <= 0.0)
-            continue;
-
-        // Add the diffuse component of the light.
-
-        color += ftemp * nearattr->Kd * lcolor;
-
-        // If this object has no specular reflection, do next light.
-
-        if (!(nearattr->flags & AT_SPECULAR))  continue;
-
-        // Calculate the light reflection vector.
-
-        ftemp *= 2.0;
-        Vector4 Refl = -ldir + (ftemp * nearnormal);  // Reflection Vector
-
-        // Calculate the cosine of the sight & reflection vectors, raised to the Phong power, for
-        // the specular reflection.
-
-        ftemp = dot(-ray.direction, Refl);
-        if (ftemp > 0.0) {
-            ftemp = pow (ftemp, nearattr->shine);
-            color += ftemp * nearattr->Ks * lcolor;
-        }
     }
-
-    REFTRAN:
 
     // If we're at the mamximum raytrace depth now, don't go any deeper.
 
